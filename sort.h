a7630264a0492888afdbf5bb2a4c06092c6c4bcb
Egor <egor.klopov2016@yandex.ru>
2022-05-10 21:58:49 +0400

    объявление функций в классе сорт

--- sort.h
+++ sort.h
@@ -1,11 +1,28 @@
 #ifndef SORT_H
 #define SORT_H
+#include <vector>
+#include <string>
+#include "region.h"
+#include <stdlib.h>
+#include <sort.h>
+#include <QList>
+#include <algorithm>
+#include <QString>
 
-
-class sort
+using namespace std;
+class Sort
 {
 public:
-    sort();
+    Sort();
+    double getMin();
+    double getMax();
+    double getMedian();
+    void read_stolb(QString stolb, QList<Region>* list);
+    QList<double> sort_list;
+private:
+    double min = 0;
+    double max = 0;
+    double median = 0;
 };
 
 #endif // SORT_H
